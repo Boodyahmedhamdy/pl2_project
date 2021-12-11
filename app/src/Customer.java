8c7bb311caadf71d03e89f8f6ebc9f33388d2d8a
unknown <abdelrahmanahmedhamdy99@gmail.com>
2021-12-11 19:55:03 +0200

    i was testing the regular expression in the main function

--- app/src/Customer.java
+++ app/src/Customer.java
@@ -1,2 +1,15 @@
+// import java.util.regex.Pattern;
+
 public class Customer extends Human{
+//    public static void main(String[] args) {
+//        System.out.println(Pattern.matches(
+//                "[A-z0-9\\._]+@(AD|CU|PM|SP)\\.(eventus)\\.\\w{3}",
+//                "ahmedhassan_200.007@PM.eventus.net"));
+//
+//        System.out.println(Pattern.matches(
+//                "[A-z0-9\\._]+@(AD|CU|PM|SP)\\.(eventus)\\.\\w{3}",
+//                "nourhan.ahmed.eventus@CU.eventus.sss"));
+//    }
+
+
 }
